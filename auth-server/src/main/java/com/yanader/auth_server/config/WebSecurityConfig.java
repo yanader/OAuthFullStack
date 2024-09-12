256910d1fd9cf48564e19190947dfee866424739
ste <mrbarrow@hotmail.co.uk>
2024-09-12 21:43:54 +0100

    added config

---
+++ auth-server/src/main/java/com/yanader/auth_server/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.yanader.auth_server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.formLogin()
+                .and()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and().build();
+    }
+}
